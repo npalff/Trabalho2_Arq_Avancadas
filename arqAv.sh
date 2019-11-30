869511e82413acca6dc612df43b6d8cbadf1482a
Nicolau Pereira Alff <npalff@inf.ufrgs.br>
2019-11-30 04:13:07 -0300

    Script Update

--- arqAv.sh
+++ arqAv.sh
@@ -1,62 +1,157 @@
 #!/bin/bash
 # Nomes
-algoritmo1=nome1.c
-algoritmo2=nome2.c
-algoritmo3=nome3.c
+algoritmo1=bfs
+algoritmo2=floyd
+algoritmo3=bfsNP
+algoritmo4=floydNP
+
 
 #Compilacao
-gcc $algoritmo1 -o $algoritmo1.out
-gcc $algoritmo2 -o $algoritmo2.out
-gcc $algoritmo3 -o $algoritmo3.out
+g++ ./src/$algoritmo1.cpp -o ./exec/$algoritmo1.out -O2
+g++ ./src/$algoritmo2.cpp -o ./exec/$algoritmo2.out -O2
+g++ ./src/$algoritmo3.cpp -o ./exec/$algoritmo3.out -O2
+g++ ./src/$algoritmo4.cpp -o ./exec/$algoritmo4.out -O2
 
-#Execucao Algoritmo 1 sem perf
-mkdir Resultados
+# Binomial
+dataName = binomial
+#Execucao Algoritmo 1 
 #
  for i in {0..39};
  do
-   ./$algoritmo1.out >> ./Resultados/$algoritmo1.semPerf.txt
+   ./exec/$algoritmo1.out ./data/$dataName >> ./results/$algoritmo1/$dataName/$algoritmo1.$dataName.$i.txt
  done
 
    echo “FIM ALGORITMO 1”
 
- #Execucao Algoritmo 2 sem perf
+ #Execucao Algoritmo 2
 #
  for i in {0..39};
  do
-   ./$algoritmo2.out >> ./Resultados/$algoritmo2.semPerf.txt
+   ./exec/$algoritmo2.out ./data/$dataName >> ./results/$algoritmo2/$dataName/$algoritmo2.$dataName.$i.txt
+   echo $i
  done
  echo “FIM ALGORITMO 2”
 
- #Execucao Algoritmo 3 sem perf
+ #Execucao Algoritmo 3
 #
  for i in {0..39};
  do
-   ./$algoritmo3.out >> ./Resultados/$algoritmo3.semPerf.txt
+   ./exec/$algoritmo3.out ./data/$dataName >> ./results/$algoritmo3/$dataName/$algoritmo3.$dataName.$i.txt
  done
  echo “FIM ALGORITMO 3”
 
- #Execucao Algoritmo 1 com perf
-mkdir Resultados
+ #Execucao Algoritmo 4
 #
  for i in {0..39};
  do
-   perf stat ./$algoritmo1.out >> ./Resultados/$algoritmo1.comPerf.txt
+   ./exec/$algoritmo3.out ./data/$dataName >> ./results/$algoritmo4/$dataName/$algoritmo4.$dataName.$i.txt
+ done
+ echo “FIM ALGORITMO 4”
+
+ # complete
+dataName = complete
+#Execucao Algoritmo 1 
+#
+ for i in {0..39};
+ do
+   ./exec/$algoritmo1.out ./data/$dataName >> ./results/$algoritmo1/$dataName/$algoritmo1.$dataName.$i.txt
  done
 
    echo “FIM ALGORITMO 1”
 
- #Execucao Algoritmo 2 com perf
+ #Execucao Algoritmo 2
 #
  for i in {0..39};
  do
-   perf stat ./$algoritmo2.out >> ./Resultados/$algoritmo2.comPerf.txt
+   ./exec/$algoritmo2.out ./data/$dataName >> ./results/$algoritmo2/$dataName/$algoritmo2.$dataName.$i.txt
+   echo $i
  done
  echo “FIM ALGORITMO 2”
 
- #Execucao Algoritmo 3 com perf
+ #Execucao Algoritmo 3
 #
  for i in {0..39};
  do
-   perf stat ./$algoritmo3.out >> ./Resultados/$algoritmo3.comPerf.txt
+   ./exec/$algoritmo3.out ./data/$dataName >> ./results/$algoritmo3/$dataName/$algoritmo3.$dataName.$i.txt
  done
  echo “FIM ALGORITMO 3”
+
+ #Execucao Algoritmo 4
+#
+ for i in {0..39};
+ do
+   ./exec/$algoritmo3.out ./data/$dataName >> ./results/$algoritmo4/$dataName/$algoritmo4.$dataName.$i.txt
+ done
+ echo “FIM ALGORITMO 4”
+
+ # random250
+dataName = random250
+#Execucao Algoritmo 1 
+#
+ for i in {0..39};
+ do
+   ./exec/$algoritmo1.out ./data/$dataName >> ./results/$algoritmo1/$dataName/$algoritmo1.$dataName.$i.txt
+ done
+
+   echo “FIM ALGORITMO 1”
+
+ #Execucao Algoritmo 2
+#
+ for i in {0..39};
+ do
+   ./exec/$algoritmo2.out ./data/$dataName >> ./results/$algoritmo2/$dataName/$algoritmo2.$dataName.$i.txt
+   echo $i
+ done
+ echo “FIM ALGORITMO 2”
+
+ #Execucao Algoritmo 3
+#
+ for i in {0..39};
+ do
+   ./exec/$algoritmo3.out ./data/$dataName >> ./results/$algoritmo3/$dataName/$algoritmo3.$dataName.$i.txt
+ done
+ echo “FIM ALGORITMO 3”
+
+ #Execucao Algoritmo 4
+#
+ for i in {0..39};
+ do
+   ./exec/$algoritmo3.out ./data/$dataName >> ./results/$algoritmo4/$dataName/$algoritmo4.$dataName.$i.txt
+ done
+ echo “FIM ALGORITMO 4”
+
+ # random1250
+dataName = random1250
+#Execucao Algoritmo 1 
+#
+ for i in {0..39};
+ do
+   ./exec/$algoritmo1.out ./data/$dataName >> ./results/$algoritmo1/$dataName/$algoritmo1.$dataName.$i.txt
+ done
+
+   echo “FIM ALGORITMO 1”
+
+ #Execucao Algoritmo 2
+#
+ for i in {0..39};
+ do
+   ./exec/$algoritmo2.out ./data/$dataName >> ./results/$algoritmo2/$dataName/$algoritmo2.$dataName.$i.txt
+   echo $i
+ done
+ echo “FIM ALGORITMO 2”
+
+ #Execucao Algoritmo 3
+#
+ for i in {0..39};
+ do
+   ./exec/$algoritmo3.out ./data/$dataName >> ./results/$algoritmo3/$dataName/$algoritmo3.$dataName.$i.txt
+ done
+ echo “FIM ALGORITMO 3”
+
+ #Execucao Algoritmo 4
+#
+ for i in {0..39};
+ do
+   ./exec/$algoritmo3.out ./data/$dataName >> ./results/$algoritmo4/$dataName/$algoritmo4.$dataName.$i.txt
+ done
+ echo “FIM ALGORITMO 4”
